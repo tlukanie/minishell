ef4bb4df50114d694ac80ce9e972c75070bf48e1
Tanya <tanya@Tanyas-MacBook-Air.local>
2023-07-05 11:59:02 +0200

    added header

--- minishell.h
+++ minishell.h
@@ -1,41 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlukanie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 10:11:10 by okraus            #+#    #+#             */
-/*   Updated: 2023/07/05 11:56:51 by tlukanie         ###   ########.fr       */
+/*   Created: 2023/07/05 11:56:30 by tlukanie          #+#    #+#             */
+/*   Updated: 2023/07/05 11:56:36 by tlukanie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <minishell.h>
-
-int	main(int argc, char *argv[], char *env[])
-{
-	int	i;
-
-	i = 0;
-	if (argc < 2)
-	{
-		printf ("use at least one arg\n");
-		printf("BYE!!!!!!!!\n");
-	}
-	else
-	{
-		while (argv[i])
-		{
-			printf("%s\n", argv[i]);
-			i++;
-		}
-	}
-	i = 0;
-	while (env[i])
-	{
-		printf("%s\n", env[i]);
-		i++;
-	}
-	return (0);
-}
