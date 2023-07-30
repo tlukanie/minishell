3af2cd58fbee7e24990fee7f6fa0c127bd81c889
Ondřej Kraus <okraus42+github@pm.me>
2023-07-30 18:09:55 +0200

    fixed expansion and prompt

--- srcs/minishell/ft_expansion.c
+++ srcs/minishell/ft_expansion.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 11:17:01 by okraus            #+#    #+#             */
-/*   Updated: 2023/07/30 11:41:18 by okraus           ###   ########.fr       */
+/*   Updated: 2023/07/30 17:56:12 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,19 +55,27 @@ static char	*ft_replacevar(t_ms *ms, char *s, int *i)
 	char	*val;
 	int		j;
 
-	s[*i] = 0;
-	*i += 1;
-	j = ft_getvarlen(&s[(*i)]);
-	var = ft_getvar(&s[(*i)]);
+
+	j = ft_getvarlen(&s[(*i) + 1]);
+	var = ft_getvar(&s[(*i) + 1]);
 	val = ft_getenvval(ms, var);
+	ft_printf("j = %i\n", j);
 	if (j < 0)
-		strend = ft_stringcopy((&s[(*i) + 1]));
+		strend = ft_stringcopy((&s[(*i)]));
 	else
-		strend = ft_stringcopy((&s[(*i) + j]));
+		strend = ft_stringcopy((&s[(*i) + j + 1]));
+	s[*i] = 0;
 	strstart = ft_stringcopy((s));
+	ft_printf("start= %s\n", strstart);
+	ft_printf("val= %s\n", val);
+	ft_printf("end= %s\n", strend);
 	s = ft_strjoin(strstart, val);
 	s = ft_strjoin_freeleft(s, strend);
-	*i -= j + 2;
+	ft_printf("s = %s\n", s);
+	ft_printf("*i = %i, s = %s\n", *i, &s[(*i)]);
+	if (val)
+		*i += ft_strlen(val) - 1;
+	ft_printf("*i = %i, s = %s\n", *i, &s[(*i)]);
 	return (s);
 }
 
